9f13d3c89a2ab131ddb2926bd6d9ca95d605b695
Daniel <dan.ossy.do@gmail.com>
2018-10-23 11:27:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import sys
-from mysorter import isorter
+from mysorter import sorter
 
 def main():
     if len(sys.argv) == 3:
         file1 = sys.argv[1]
         file2 = sys.argv[2]
-        common_occurence = isorter(file1, file2)
+        common_occurence = sorter(file1, file2)
         if common_occurence:
             print("There are " +" %s "  %len(common_occurence) +" common occurrence.")
     else:
         print("USEAGE: main.py myfile1 myfile2")
 if __name__ == '__main__':
     main()
-
