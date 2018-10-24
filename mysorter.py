0cb55535a396fc3ff7bee2a63177c3467fa709b0
Daniel <dan.ossy.do@gmail.com>
2018-10-24 23:55:46 +0200

    Add files via upload

--- mysorter.py
+++ mysorter.py
@@ -1,11 +1,11 @@
 import os
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 def sorter(file1, file2):
-    '''finds common occurences between 2 files'''
+    '''finds common occurences between 2 plain files'''
     if os.path.isfile(file1) and os.path.isfile(file2):
         if os.access(file1, os.R_OK) and os.access(file2, os.R_OK):
-           reader = {(line1, line2) for line1 in open(file1, 'rb').readlines() for line2 in open(file2, 'rb').readlines() if line1 == line2}
+           reader = {(line1, line2) for line1 in open(file1, 'rb').readlines() for line2 in open(file2, 'rb').readlines() if line1.split() == line2.split()}
            return reader
         else:
             print("Ensure you have permission to the two files.")
