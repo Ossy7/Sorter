65c3ebab011ca14674d1af83f8c340d6f7f951be
Daniel <dan.ossy.do@gmail.com>
2019-11-09 10:12:09 +0100

    Add files via upload
    Mysorter.py now includes a function with which you can outline the parts of speech in a given file.

--- mysorter.py
+++ mysorter.py
@@ -1,5 +1,6 @@
 import os
-__version__ = "1.0.3"
+from nltk import word_tokenize, pos_tag
+__version__ = "1.0.4"
 
 def sorter(file1, file2):
     '''finds common occurences between 2 plain files'''
@@ -20,6 +21,20 @@ def word_counter(myfile):
             number_words = [len(word.split()) for word in open(myfile, 'rb').readlines()]
             wCounter = sum(number_words)
             return wCounter
+        else:
+            print("Ensure you have permission to the file.")
+    else:
+        print("Ensure the file is present in the current directory.")
+
+def Parts_Speech(myfile):
+    '''Finds the parts of speech in a given file.'''
+    if os.path.isfile(myfile):
+        if os.access(myfile, os.R_OK):
+            reader = open(myfile, 'rb').readlines()
+            reader = str(reader)
+            tokens = word_tokenize(reader)
+            pos = pos_tag(tokens)
+            return pos
         else:
             print("Ensure you have permission to the file.")
     else:
